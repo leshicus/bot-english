0f2541826c1ca496b629f85a923be9be4de3ca43
leshicus <leshicus@gmail.com>
2019-01-01 12:08:19 +0300

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.TELEGRAM_TOKEN;
+
+if (!token) throw new Error('Telegram Bot Token not provided');
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  console.log('onText', msg, match);
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', msg => {
+  const chatId = msg.chat.id;
+  console.log('message', msg);
+  bot.sendMessage(chatId, 'received');
+});
