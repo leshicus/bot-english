9967700e1c7ba5b1eff369d1b70a3456f26a1053
leshicus <leshicus@gmail.com>
2019-01-04 16:56:23 +0300

    some result

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// @flow
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+
+const { start, startLesson, onCallbackQuery, message } = require('./src/botCommands');
 
 const token = process.env.TELEGRAM_TOKEN;
 
@@ -7,16 +10,32 @@ if (!token) throw new Error('Telegram Bot Token not provided');
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  console.log('onText', msg, match);
-  const chatId = msg.chat.id;
-  const resp = match[1];
+// const keyboard = {
+//   keyboard: [
+//     [
+//       { text: 'Sample text' },
+//       { text: 'Second sample', callback_data: 'COMMAND_TEMPLATE1' },
+//     ],
+//     [ 'Keyboard' ],
+//     [ "I'm robot" ],
+//   ],
+// };
 
-  bot.sendMessage(chatId, resp);
-});
+// /\/start (.+)/
+bot.onText(/\/start/, (msg, match) => start(bot, msg, match));
 
-bot.on('message', msg => {
-  const chatId = msg.chat.id;
-  console.log('message', msg);
-  bot.sendMessage(chatId, 'received');
-});
+bot.onText(/^\/\d+$/, (msg, match) => startLesson(bot, msg, match));
+
+bot.on('message', msg => onMessage(bot, msg));
+
+bot.on('callback_query', query => onCallbackQuery(bot, query));
+
+// bot.removeReplyListener(replyListenerId);
+
+// bot.editMessageText('Выберете шаблон',{
+//   chat_id: chat.id,
+//   message_id:message_id,
+//   reply_markup: {
+//       inline_keyboard
+//   }
+// })
